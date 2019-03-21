19b1cc2365ba7e46fa490af3ae934d5619708f7f
nkumar0727 <nkumar0727@gmail.com>
2019-03-21 13:23:44 -0500

    fixed CORS, added textures and incorporated multiple lighting

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 
 if (typeof require === 'function') {
-    var THREE = require('three')
+    var THREE = require('three');
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// CREATING THE SCENE
-///////////////////////////////////////////////////////////////////////////////
-
-var scene = new THREE.Scene()
 /**
  *  PerspectiveCamera(fieldOfView, aspectRatio, nearClippingPane, farClippingPane)
  *      + fieldOfView -- extent of the seen seen on display @ given moment (degrees)
@@ -18,74 +13,44 @@ var scene = new THREE.Scene()
  *          --> can modify these two for performance gains
  * 
  */
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-
-/**
- *  WebGLRenderer()
- *      + There are other renderers than this one, in case browser doesn't support WebGL
- */
-var renderer = new THREE.WebGLRenderer()
-
-/**
- *  WebGLRenderer.setSize(widthToRender, heightToRender)
- *      --> can modify these two for performance gains
- *  WebGLRenderer.setSize(widthToRender, heightToRender, updateStyle)
- *      + updateStyle -- false: renders app at half resolution, true: renders app at full resolution
- */
-renderer.setSize(window.innerWidth, window.innerHeight)
-
-/**
- *   Adds the <canvas> element to the DOM. Renderer uses this element to display scene.
- */
-document.body.appendChild(renderer.domElement)
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
+var renderer = new THREE.WebGLRenderer({
+    alpha: false
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 
-
-/**
- *  Object contains all vertices and faces of a cube.
- *  BoxGeometry(length, height, width)
- */
-var geometry = new THREE.BoxGeometry(1, 1, 1)
-
-/**
- *  Object which helps add color.
- */
-var material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-})
-
-/**
- *  Object which takes a geometry, and applies material to it.
- */
-var cube = new THREE.Mesh(geometry, material)
-
-/**
- *  By default, scene.add() puts object in coordinates (0, 0, 0)
- */
-scene.add(cube)
-
-/**
- *  Move camera out, so that the camera is not inside the cube at (0, 0, 0)
- */
-camera.position.z = 5
+var cubeMesh;
+var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+let cubeColor = new THREE.Color("#7833aa");
 
 
-///////////////////////////////////////////////////////////////////////////////
-// RENDERING THE SCENE
-// ANIMATING THE CUBE
-///////////////////////////////////////////////////////////////////////////////
+let textureLoader = new THREE.TextureLoader();
+textureLoader.setCrossOrigin("anonymous");
+textureLoader.load('https://s3.us-east-2.amazonaws.com/aws-practice-nikhil-bucket/brick-texture.gif', function (texture) {
+    let material = new THREE.MeshPhongMaterial({
+        color: cubeColor.getHex(),
+        bumpMap: texture
+    });
+    cubeMesh = new THREE.Mesh(cubeGeometry, material);
+    scene.add(cubeMesh);
+    render();
+});
 
-/**
- *  window.requestAnimationFrame()
- *      + redraws the scene every time the screen is refreshed
- *      + when user navigates to another browser tab, animation pauses (saves processing power)
- */
-function animate() {
-    requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    renderer.render(scene, camera)
+let lightArr = [];
+for (let i = 0; i < 4; ++i) {
+    lightArr.push(new THREE.PointLight('white', 0.8));
+    lightArr[i].position.set((i % 2) * 10, 10, ((i+1) % 2) * 10);
+    scene.add(lightArr[i]);
 }
-animate()
+
+
+var render = function () {
+    requestAnimationFrame(render);
+    cubeMesh.rotation.x += 0.01;
+    cubeMesh.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
